ca562bbc323db095d54e7c9aac58ffa84f9869b5
mconway <michael_conway@unc.edu>
2014-05-29 09:40:27 -0400

    remove parameters to allow building via maven

--- src/databook/Activator.java
+++ src/databook/Activator.java
@@ -1,51 +1,41 @@
 package databook;
 
-import java.io.InputStream;
-
-import org.irods.jargon.core.packinstr.InxVal;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
-import org.osgi.framework.ServiceReference;
 import org.osgi.framework.ServiceRegistration;
 
 import databook.listener.Indexer;
-import databook.listener.ModelUpdater;
 import databook.listener.service.IndexingService;
-import databook.listener.service.MessagingService;
 import databook.local.model.RDFDatabase;
-import databook.local.model.RDFServiceWrapper;
 
 public class Activator implements BundleActivator {
-	ServiceRegistration<MessagingService> sr;
-	ServiceRegistration<IndexingService> inxSvc;
+	ServiceRegistration sr;
+	ServiceRegistration inxSvc;
 	Indexer i;
 	RDFDatabase database;
 	IndexingService ts;
 
+	@Override
 	public void start(BundleContext context) throws Exception {
-		/*ModelUpdater s = new ModelUpdater();
-		if(sr == null) {
-			sr = context.registerService(MessageHandler.class, s, null);
-		}
-		if(inxSvc == null) {
-			inxSvc = context.registerService(IndexingService.class, s, null);
-		}*/
+		/*
+		 * ModelUpdater s = new ModelUpdater(); if(sr == null) { sr =
+		 * context.registerService(MessageHandler.class, s, null); } if(inxSvc
+		 * == null) { inxSvc = context.registerService(IndexingService.class, s,
+		 * null); }
+		 */
 		System.out.println("indexing framework started");
 	}
-	
+
+	@Override
 	public void stop(BundleContext context) throws Exception {
-		/*if(sr != null) {
-			sr.unregister();
-			sr = null;
-		}
-		
-		if(inxSvc != null) {
-			inxSvc.unregister();
-			inxSvc = null;
-		}*/
+		/*
+		 * if(sr != null) { sr.unregister(); sr = null; }
+		 * 
+		 * if(inxSvc != null) { inxSvc.unregister(); inxSvc = null; }
+		 */
 
 		System.out.println("indexing framework stopped");
-		
+
 	}
 
 }
